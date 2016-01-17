3c4b77f316a4698065d324fadd0fc239484cbbbe
Hao Chang <yngwie.chang@gmail.com>
2016-01-17 18:06:38 +1100

    [FIX] Fixed a problem of the timeline going out of its container when zooming and refactored the JS code to improve reusability.

--- app/assets/scripts/src/app.js
+++ app/assets/scripts/src/app.js
@@ -1,14 +1,26 @@
 'use strict';
 
 (function(window, $) {
-    /******************** timeline | drag ********************/
     $(function() {
-        var _isCursorDown = false,
-            _cursorPosition = 0,
-            _$window = $(window),
+        /******************** private variables ********************/
+        // common
+        var _$window = $(window),
             _$timelineContainer = $('#timeline-container'),
-            _$timeline = $('#timeline');
+            _$timeline = $('#timeline'),
+            _$timelineItem = $('.ch-timeline__item, .ch-timeline__item_inverted');
 
+        // for timeline drag
+        var _isCursorDown = false,
+            _cursorPosition = 0;
+
+        // for timeline zoom
+        var _gapProperty = 'marginBottom',
+            _gapRange = {
+                min: 0,
+                max: 300
+            };
+
+        /******************** timeline | drag ********************/
         _$timeline.on('mousedown', function(e) {
             // primary button only
             if (e.which !== 1) {
@@ -26,53 +38,69 @@
                 return;
             }
 
-            var cursorPositionNew = e.pageY,
-                cursorPositionDifference = cursorPositionNew - _cursorPosition,
-                containerHeight = _$timelineContainer.outerHeight(),
+            // the delta to the timeline height
+            var delta = e.pageY - _cursorPosition;
+
+            // properly place the timeline
+            _adjustTimelinePosition(delta);
+
+            // upgrade the global cursor position
+            _cursorPosition = e.pageY;
+        });
+
+        /******************** timeline | zoom ********************/
+        _$timeline.on('mousewheel', function(e) {
+            var delta = e.deltaY,
+                gap = _$timelineItem.cssInt(_gapProperty),
+                gapNew = gap + delta;
+
+            // zoom within the range
+            if (gapNew < _gapRange.min || gapNew > _gapRange.max) {
+                return;
+            }
+
+            // zoom
+            _$timelineItem.css(_gapProperty, gapNew + 'px');
+
+            // ensure the timeline doesn't go beyond its container when the height changes
+            _adjustTimelinePosition();
+        });
+
+        /******************** private functions ********************/
+        /**
+         * Adjusts the timeline position.
+         *
+         * During dragging or zooming the timeline, there is a chance for
+         * the timeline to be placed outside of its container leaving a blank space.
+         *
+         * This function ensures the timeline is placed properly.
+         *
+         * @param delta The delta to the timeline height
+         */
+        function _adjustTimelinePosition(delta) {
+            delta = delta || 0;
+
+            var containerHeight = _$timelineContainer.outerHeight(),
                 containerPositionTop = _$timelineContainer.offset().top,
                 containerPositionBottom = containerPositionTop + containerHeight,
                 timelineHeight = _$timeline.outerHeight(),
                 timelinePositionTop = _$timeline.offset().top,
                 timelinePositionBottom = timelinePositionTop + timelineHeight,
-                timelinePositionTopNew = timelinePositionTop + cursorPositionDifference,
-                timelinePositionBottomNew = timelinePositionBottom + cursorPositionDifference;
+                timelinePositionTopNew = timelinePositionTop + delta,
+                timelinePositionBottomNew = timelinePositionBottom + delta;
 
-            // beyond the borders
+            // ensure the timeline doesn't go beyond its container when the height changes
             if (timelinePositionTopNew > containerPositionTop) {
                 timelinePositionTopNew = containerPositionTop;
             } else if (timelinePositionBottomNew < containerPositionBottom) {
                 timelinePositionTopNew = containerPositionBottom - timelineHeight;
             }
 
-            // drag to position
+            // move the timeline vertically
             _$timeline.offset({
                 top: timelinePositionTopNew
             });
-            _cursorPosition = e.pageY;
-        });
-    });
-
-    /******************** timeline | zoom ********************/
-    $(function() {
-        var _gapProperty = 'marginBottom',
-            _gapRange = {
-                min: 0,
-                max: 300
-            },
-            _$timeline = $('#timeline'),
-            _$timelineItem = $('.ch-timeline__item, .ch-timeline__item_inverted');
-
-        _$timeline.on('mousewheel', function(e) {
-            var delta = e.deltaY,
-                gap = _$timelineItem.cssInt(_gapProperty),
-                gapNew = gap + delta;
-
-            if (gapNew < _gapRange.min || gapNew > _gapRange.max) {
-                return;
-            }
-
-            _$timelineItem.css(_gapProperty, gapNew + 'px');
-        });
+        }
     });
 
     /******************** jQuery prototype ********************/
